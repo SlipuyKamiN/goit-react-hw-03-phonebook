ea952663eccb0f84148220aa60039bc41dac3baa
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-05-02 16:31:23 +0200

    does homework

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,14 +11,28 @@ import { Filter } from 'components/Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (savedContacts.length) {
+      this.setState({
+        contacts: savedContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const previousContacts = prevState.contacts;
+    const updatedContacts = this.state.contacts;
+
+    if (updatedContacts.length !== previousContacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+    }
+  }
 
   handleFormSubmit = ({ name, number }) => {
     const normalizedName = name.toLowerCase();
